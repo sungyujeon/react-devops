b5bc3c75cf30722fee5e31ab7942e604a3680ace
sungyujeon <sungyujeon91@gmail.com>
2021-06-29 23:51:28 +0900

    Test a github actions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          테스트2
+          테스트(using actions)
         </p>
         <a
           className="App-link"
