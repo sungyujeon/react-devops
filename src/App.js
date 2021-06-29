84094f7d3357becb12d47ce36a3277d32f4062f7
sungyujeon <sungyujeon91@gmail.com>
2021-06-29 23:49:46 +0900

    Test a deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          테스트
+          테스트2
         </p>
         <a
           className="App-link"
